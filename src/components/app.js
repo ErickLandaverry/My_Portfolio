df5a8e4e3344c22da9a6d327cbd48c1de72aae93
landy <landy.erick20@gmail.com>
2018-04-26 17:51:36 -0600

    Initial Commit

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="name_header">
+        <h1>Erick Landaverry</h1>
+      </div>
+    );
+  }
+}
